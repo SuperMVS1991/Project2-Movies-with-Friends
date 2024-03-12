66deb4a8668bf4cdd42ca3857ba5508ae0a333c1
HeidiHarmony <tenibrae@gmail.com>
2024-03-11 22:15:13 -0400

    Troubleshooting server connection, added some required dependencies, fixed some minor typos

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
+// const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-// Import the connection object
-const sequelize = require("./config/connection");
-// check with tutor if file path is correct above
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3000;
+
+const hbs = exphbs.create({ helpers });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // Connect to the database before starting the Express.js server
 sequelize.sync().then(() => {
