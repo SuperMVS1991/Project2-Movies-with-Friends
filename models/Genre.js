f9870c93a6491aa60b6ddcdb8fac9fc07a6d9273
HeidiHarmony <tenibrae@gmail.com>
2024-03-13 23:26:03 -0400

    Created the equiv of main.handlebars, updated landing page with the sign in form/visibility

---
+++ models/Genre.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/connection');
+
+const Genre = sequelize.define('Genre', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+}, {
+    tableName: 'genres',
+    timestamps: false,
+    underscored: true,
+});
+
+
+module.exports = Genre;
