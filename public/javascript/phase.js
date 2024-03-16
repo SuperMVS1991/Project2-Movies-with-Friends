51aa002ab5426233bf6dbbf45ac36754aaeeb4e3
HeidiHarmony <tenibrae@gmail.com>
2024-03-15 21:10:04 -0400

    bedtime commit - adding code to phase.js to display the month and genre

--- public/javascript/phase.js
+++ public/javascript/phase.js
@@ -1,17 +1,22 @@
+const currentDate = new Date(); // Use the current date
+
 window.addEventListener("DOMContentLoaded", function() {
     // Your code here will run when the entire page, including the Handlebars template, is fully loaded
     let phaseName = getCurrentPhase();
     let phaseDisplay = document.getElementById('phase-display');
     phaseDisplay.innerHTML = phaseName;
+
+    let monthName = getCurrentMonth();
+    let monthDisplay = document.getElementById('current-month-display');
+    monthDisplay.innerHTML = monthName;
 });
 
 /* Checks the current date to determine the phase of site activity */
 function getCurrentPhase() {
-    const currentDate = new Date(); // Use the current date
     const phases = [
-        { name: "Nomination", start: 1, end: 5 },
+        { name: "Nominating", start: 1, end: 5 },
         { name: "Voting", start: 6, end: 7 },
-        { name: "Discussion", start: 8, end: 31 }
+        { name: "Discussing", start: 8, end: 31 }
     ];
     const currentDay = currentDate.getDate();
   
@@ -22,3 +27,25 @@ function getCurrentPhase() {
     }
     return "No current phase"; // Return this if no phase matches the current date
 }  
+
+/* Returns the current month */
+function getCurrentMonth() {
+    const currentMonth = currentDate.getMonth();
+    const months = [
+        "January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"
+    ];
+    const monthName = months[currentMonth];
+    return monthName;
+}
+
+/* Returns the month's movie genre */
+function getMonthGenre() {
+    const currentMonth = currentDate.getMonth();
+    const genres = [
+        "Drama", "Romance", "Comedy", "Action", "Horror", "Documentary",
+        "Science Fiction", "Fantasy", "Mystery", "Thriller", "Adventure", "Animation"
+    ];
+    const genre = genres[currentMonth];
+    return genre;
+}
