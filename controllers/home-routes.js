73b44a5fe2805f7ceeb4ac641b10e7fe31d88b1d
SuperMVS1991 <150755455+SuperMVS1991@users.noreply.github.com>
2024-03-18 14:01:09 -0400

    updated home-routes.js file

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,5 @@
 const router = require("express").Router();
-const {
-  User,
-  Movie,
-} = require("../models");
+const { Movie, User } = require("../models");
 const withAuth = require("../utilities/auth");
 
 router.get("/", async (req, res) => {
@@ -66,6 +63,31 @@ router.get("/profile", withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
+});
+
+
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("signup");
+}
+);
+
+router.get("/add", withAuth, (req, res) => {
+  res.render("add");
+});
+
 
 module.exports = router;
 
