29ef2e238f287febcb8097923afdd090703c5690
HeidiHarmony <tenibrae@gmail.com>
2024-03-19 08:58:55 -0400

    landing finally renders in a separate layout called main-withoutauth.handlebars, tweaked formatting to show as much content as possible near the top of the page

--- utilities/auth.js
+++ utilities/auth.js
@@ -1,14 +1,11 @@
-const express = require('express');
-const app = express();
-
-const withAuth = app.get('/', (req, res) => {
+const withAuth = (req, res, next) => {
   // If the user isn't logged in, redirect them to the landing route
   if (!req.session.logged_in) {
-    res.redirect('/landing');
+    res.render('landing', { layout: 'main-withoutauth' });
   } else {
     // If the user is logged in, render the welcome view with the main layout
     res.render('welcome', { layout: 'main' });
   }
-});
-  
-  module.exports = withAuth;  
+};
+
+module.exports = withAuth;
