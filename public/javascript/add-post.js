a03d05bb2bcfd81c028675d2f76ed43d08b2a3a1
HeidiHarmony <tenibrae@gmail.com>
2024-03-18 20:21:15 -0400

    landing page signup button goes to the sign up view

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,4 +1,1 @@
-document.getElementById('signupBtn').addEventListener('click', function() {
-  // Redirect to the desired view
-  window.location.href = 'signup'; // Change 'view.html' to the URL of your desired view/page
-});
+
--- public/javascript/mwf-misc.js
+++ public/javascript/mwf-misc.js
@@ -4,18 +4,33 @@ const loginButton = document.getElementById('landing-showlogin');
 const memberSignin = document.getElementById('member-signin');
 
 
-document.getElementById('action-button').addEventListener('click', function() {
+loginButton.addEventListener('click', function() {
+  memberSignin.classList.toggle('visible-form');
+  console.log('clicked-member-login');
+}
+);
+
+// Button to go to signup view
+
+document.getElementById('signup-button').addEventListener('click', function() {
   // Redirect to the desired view
-  window.location.href = 'discussion'; // Change 'view.html' to the URL of your desired view/page
+  window.location.href = 'signup'; // Change 'view.html' to the URL of your desired view/page
 });
 
 // Code to confirm password match
 
-const password = document.getElementById('password');
+const password = document.getElementById('password-signup');
 const confirmPassword = document.getElementById('confirm-password');
-const signUpButton = document.getElementById('signup-button');
+const signUpButton = document.getElementById('submit-signup');
 
 signUpButton.addEventListener('click', function() {
+  // Redirect to the desired view
+  window.location.href = 'signup'; // Change 'view.html' to the URL of your desired view/page
+});
+
+const submitSignup = document.getElementById('submit-signup');
+
+submitSignup.addEventListener('click', function() {
   if (password.value !== confirmPassword.value) {
     alert('Passwords do not match');
     return;
